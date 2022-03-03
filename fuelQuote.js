fe694f311f64cced3b5b225c930e967443823821
juanitacorrea <38385180+juanitacorrea@users.noreply.github.com>
2022-03-03 15:35:46 -0600

    .js files needed

---
+++ fuelQuote.js
@@ -0,0 +1,4 @@
+//take all inputs from fuel QUOTE HTML
+//IN FUEL QUOTOE HTML GIVE ELEMENTS A SPECIFIC ID
+//create a function that calculates the total amount due (gallons*price)
+//then output onto a text file the data that was inputted into the fuel quote (and final quote that was calculated from function)
---
+++ login.js
@@ -0,0 +1,5 @@
+//in login, check the inputs from username and password textboxes from the login.html (reference murtaza)
+//craete a function to creat a query to 
+    //-look for username 
+    //-if username is in there look for password in the same line text file (matches up account) 
+    //-if username was not in there say that "account doesn't exist" and give them a prompt to go back to sign up
