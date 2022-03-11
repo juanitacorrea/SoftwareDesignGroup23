c010118da8ead106725cff30e825ca4dd5923a6a
murtazaszaidi <98568084+murtazaszaidi@users.noreply.github.com>
2022-03-11 14:35:57 -0600

    Everyhting working except for fuelQuote styling

--- login.js
+++ login.js
@@ -3,3 +3,95 @@
     //-look for username 
     //-if username is in there look for password in the same line text file (matches up account) 
     //-if username was not in there say that "account doesn't exist" and give them a prompt to go back to sign up
+    window.sessionStorage;
+    var tester = 0;
+    function checkForm1()
+    {
+        //assigning all the values that were inputted into the text boxes
+       const name = document.getElementById('usernameInputBoxes');
+       const password = document.getElementById('passwordInputBoxes');
+       const passwordCheckInputBoxes = document.getElementById('passwordCheckInputBoxes');
+       const error = document.getElementById('formErrors');
+       let errorsFound = false;
+       
+       if (!name.value) //makes sure that you inputted something in the name box
+       {
+          errorsFound = true;
+          name.style = "border: 2px solid red;";
+          error.style = "display: block";
+          const nameErr = document.createElement("li");
+          nameErr.textContent = "Missing full name.";
+          error.appendChild(nameErr);
+       }
+    
+       if (!password.value) //makes sure that you inputted something in the name box
+       {
+          errorsFound = true;
+          name.style = "border: 2px solid red;";
+          error.style = "display: block";
+          const nameErr = document.createElement("li");
+          nameErr.textContent = "Missing password.";
+          error.appendChild(nameErr);
+       }
+       
+       isInLocal(name, password);
+       
+       if (errorsFound === false && tester != 0)
+       {
+          error.style = "display: block";
+          name.style = "border: 1px solid #aaa;";
+          password.style = "border: 1px solid #aaa;";
+          sessionStorage.setItem("currentloggedin", name.value);
+          window.location.href = "menu.html";
+       }
+       
+    }
+    //insert a bool function here that can iterate through the array and tell you whether or not the username-password pair is in there
+
+    function isInLocal(username, password)   // to check the login 
+    {
+        /*var username = document.getElementById("username").value
+        var password = document.getElementById("password"). value 
+        console.log("your username is" + username + " and your password is" + password)
+        copyOfObjPeople = JSON.parse(localStorage.getItem("users")); //making a copy of the array that was made in register.js
+        for (index = 0; index < copyOfObjPeople.length; index++)
+        {
+            if(username === copyOfObjPeople[index].username && password === copyOfObjPeople[index].password)
+            {
+                console.log(username + "logged in successfully")
+                return
+            }
+        }*/
+        copyOfObjPeople = JSON.parse(localStorage.getItem("users"));
+        copyOfObjPeople.forEach(function(objPeople) //u can use this to iterate through the array in your bool function
+        {
+            var usrnm = objPeople.username;
+            var pswd = objPeople.password;
+            //console.log(usrnm + "   " + pswd + "\n");
+            if(username.value === usrnm && password.value === pswd)
+            {
+                console.log(usrnm + "logged in sucscessfully");
+                
+                tester = 1;
+            }
+        });
+        //console.log(username.value + "failed login");
+        //console.log("incorrect password or username")
+    }  
+
+  
+    document.getElementById("submit").addEventListener("click", function(event) 
+    {
+        checkForm1(); 
+
+        //this section is just outputting the usernames and passwords into the console just so we can see that they transferred over properly
+        //console.log(copyOfObjPeople);
+       /* copyOfObjPeople.forEach(function(objPeople) //u can use this to iterate through the array in your bool function
+        {
+            var usrnm = objPeople.username;
+            var pswd = objPeople.password;
+            console.log("username: " + usrnm + " Password: " + pswd);
+        });*/
+    //Prevent default form action. DO NOT REMOVE THIS LINE
+        event.preventDefault();
+    });
---
+++ profileManager.js
@@ -0,0 +1,98 @@
+window.sessionStorage;
+function checkForm()
+{
+   const fullname = document.getElementById('fullNameInputBoxes');
+   const addrLine1 = document.getElementById('address1InputBoxes');
+   const addrLine2 = document.getElementById('addres2InputBoxes');
+   const city = document.getElementById('cityInputBoxes');
+   const state = document.getElementById('stateInputBoxes');
+   const zip = document.getElementById('zipcodeInputBoxes');
+   const error = document.getElementById('formErrors');
+   let errorsFound = false;
+
+   
+   if (!fullname.value) //makes sure that you inputted something in the name box
+   {
+      errorsFound = true;
+      fullname.style = "border: 2px solid red;";
+      error.style = "display: block";
+      const nameErr = document.createElement("li");
+      nameErr.textContent = "Missing full name.";
+      error.appendChild(nameErr);
+   }
+
+   if (!addrLine1.value) //checks to make sure that the password has numbers in it
+   {
+      errorsFound = true;
+      addrLine1.style = "border: 2px solid red;";
+      error.style = "display: block";
+      const passErr4 = document.createElement("li");
+      passErr4.textContent = "Password must contain at least one digit.";
+      error.appendChild(passErr4);
+   }
+
+   if (!addrLine2.value) //checks to make sure that the password has numbers in it
+   {
+      errorsFound = true;
+      addrLine2.style = "border: 2px solid red;";
+      error.style = "display: block";
+      const passErr4 = document.createElement("li");
+      passErr4.textContent = "Password must contain at least one digit.";
+      error.appendChild(passErr4);
+   }
+
+   if (!city) //checks to make sure that the password has numbers in it
+   {
+      errorsFound = true;
+      city.style = "border: 2px solid red;";
+      error.style = "display: block";
+      const passErr4 = document.createElement("li");
+      passErr4.textContent = "Password must contain at least one digit.";
+      error.appendChild(passErr4);
+   }
+
+   if (!zip) //checks to make sure that the password has numbers in it
+   {
+      errorsFound = true;
+      city.style = "border: 2px solid red;";
+      error.style = "display: block";
+      const passErr4 = document.createElement("li");
+      passErr4.textContent = "Password must contain at least one digit.";
+      error.appendChild(passErr4);
+   }
+
+   if (errorsFound === false) 
+   {
+      error.style = "display: none";
+      fullname.style = "border: 1px solid #aaa;";
+      addrLine1.style = "border: 1px solid #aaa;";
+      addrLine2.style = "border: 1px solid #aaa;";
+      city.style = "border: 1px solid #aaa;";
+      state.style = "border: 1px solid #aaa;";
+      zip.style = "border: 1px solid #aaa;";
+      var currerntUser = sessionStorage.getItem('currentloggedin');
+      var copyOfObjPeople = JSON.parse(localStorage.getItem("users"));
+      copyOfObjPeople.forEach(function(objPeople) //u can use this to iterate through the array in your bool function
+      {
+         var usrnm = objPeople.username;
+         //console.log(usrnm + "   " + pswd + "\n");
+         if(currerntUser === usrnm)
+         {
+             console.log("Currently editing: " + usrnm);  
+             objPeople.fullName = fullname.value;
+             objPeople.addrLine1 = addrLine1.value;
+             objPeople.addrLine2 = addrLine2.value;
+             objPeople.city = city.value;
+             objPeople.state = "temp";
+             objPeople.zip = zip.value;
+         }
+      });
+      localStorage.setItem('users', JSON.stringify(copyOfObjPeople));
+   }
+}
+document.getElementById("submit").addEventListener("click", function(event) 
+{
+    checkForm(); 
+   //Prevent default form action. DO NOT REMOVE THIS LINE
+    event.preventDefault();
+});
