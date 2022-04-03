3fbe71fe8dad1d29b8d68d3925c683c65cc86d41
murtazaszaidi <98568084+murtazaszaidi@users.noreply.github.com>
2022-04-03 11:29:30 -0500

    cleaned up comments and currentUser

--- app.js
+++ app.js
@@ -16,7 +16,6 @@ db.once('open', function(callback){
 	console.log("connection succeeded");
 })
 
-// var app=express()
 app.use(express.static(__dirname));
 
 app.use(bodyParser.json());
@@ -27,7 +26,13 @@ app.use(bodyParser.urlencoded({
 var spawn = require('child_process').spawn;
 
 
-app.post('/register', function(req,res){
+//*************************************//
+//**********  POST FUNCTIONS  *********//
+//*************************************//
+
+//** REGISTER POST FUNCTION **//////////////////////////////////////////////////
+app.post('/register', function(req,res)
+{
     var username = req.body.username;
     var psw = req.body.psw;
     var pswCheck = req.body.pswCheck;
@@ -68,32 +73,64 @@ app.post('/register', function(req,res){
      }	
 });
 
+
+
+//** LOGIN POST FUNCTION **//////////////////////////////////////////////////
 app.post('/login', function(req,res)
 {
     var uname = req.body.uname;
     var pswd = req.body.pswd;
 
+    var fullName = "";
+    var addrLine1 = ""; 
+    var addrLine2 = "";
+    var city = "";
+    var state = "";
+    var zip = "";
+
     console.log("username:" + uname + " password: " + pswd);
 
+    const copyOfUserdataCollection = require('./userdata.json');
     const copyOfLoginCollection = require('./loginInfo.json');
     if(login.isInDB(uname, pswd, copyOfLoginCollection, 0) == 1)
     {
         console.log("You've successfully logged in!")
+        //if userdata already exists, set these as currentUser values
+        copyOfUserdataCollection.forEach(function(objPeople)
+        {
+            if(uname == objPeople.username)
+            {
+                fullName = objPeople.fullName;
+                addrLine1 = objPeople.addrLine1; 
+                addrLine2 = objPeople.addrLine2;
+                city = objPeople.city;
+                state = objPeople.city;
+                zip = objPeople.zip;
+            }
+        });
+
         var data = 
         {
             $set:
-        
-        {
-            "username":uname,
-            "fullName": "",
-            "addrLine1": "",
-            "addrLine2": "",
-            "city": "",
-            "state": "",
-            "zip": "",
-            "bVal": "val"
+            {
+                "username":uname,
+                "fullName": fullName,
+                "addrLine1": addrLine1,
+                "addrLine2": addrLine2,
+                "city": city,
+                "state": state,
+                "zip": zip,
+                "bVal": "val",
+                "mostRecentGal": "",
+                "mostRecentTotal": "",
+                "mostRecentSugPrice": ""
+            }
         }
-        }
+        // db.collection('currentUser').insertOne(data,function(err, collection)
+        // {
+        //     if (err) throw err;
+        //     console.log("Current user data updated");
+        // });
 
         var myquery = { bVal: "val" };
 
@@ -109,7 +146,8 @@ app.post('/login', function(req,res)
     }
 });
 
-///////////////////////////////////////////////
+
+//** FUEL QUOTE FORM POST FUNCTION **//////////////////////////////////////////////////
 app.post('/fuelQuoteForm1', function(req,res)
 {
     var address = "";
@@ -121,13 +159,16 @@ app.post('/fuelQuoteForm1', function(req,res)
 
     copyOfCurrentUser.forEach(function(objPeople)
     {
+        fullname = objPeople.fullName;
         username = objPeople.username;
         address = objPeople.addrLine1;
+        address2 = objPeople.addrLine2;
         city = objPeople.city;
         state = objPeople.state;
         zipcode = objPeople.zip;
 
     });
+
     var gallons = req.body.gallonsR;
     var date = req.body.deliveryDate;
     var suggPrice = "2.80";
@@ -149,9 +190,39 @@ app.post('/fuelQuoteForm1', function(req,res)
         ls = spawn('mongoexport',['--db', 'gfg','--collection', 'quotes', '--jsonArray', '--out', 'quotes.json']);
     });
 
+    //update the current user with the most recent quote info
+    var data = 
+        {
+            $set:
+            {
+                "username":username,
+                "fullName": fullname,
+                "addrLine1": address,
+                "addrLine2": address2,
+                "city": city,
+                "state": state,
+                "zip": zipcode,
+                "bVal": "val",
+                "mostRecentGal": gallons,
+                "mostRecentTotal": total,
+                "mostRecentSugPrice": suggPrice
+            }
+        }
+    var myquery = { bVal: "val" };
+
+    db.collection('currentUser').updateOne(myquery, data, function(err, res) 
+    {
+        if (err) throw err;
+        console.log("Current user data updated");
+    });
+        
+    ls = spawn('mongoexport',['--db', 'gfg','--collection', 'currentUser', '--jsonArray', '--out', 'currentUser.json']);
     return res.redirect('/finalSubmissionForm');
 });
-///////////////////////////////////////////////////
+
+
+
+//** CLIENT PROFILE NAME POST FUNCTION **//////////////////////////////////////////////////
 app.post('/clientProfileName', function(req,res)
 {
     var fullname = req.body.name;
@@ -191,7 +262,14 @@ app.post('/clientProfileName', function(req,res)
     return res.redirect('/menu');
  });
 
- app.get('/fuelQuoteForm', function(req, res){
+
+//*************************************//
+//**********  GET FUNCTIONS  **********//
+//*************************************//
+
+//** FUEL QUOTE FORM GET FUNCTION **//////////////////////////////////////////////////
+ app.get('/fuelQuoteForm', function(req, res)
+ {
 
     var address = "";
     var city  = "";
@@ -202,14 +280,13 @@ app.post('/clientProfileName', function(req,res)
 
     copyOfCurrentUser.forEach(function(objPeople)
     {
-        //username = objPeople.username;
         address = objPeople.addrLine1;
         city = objPeople.city;
         state = objPeople.state;
         zipcode = objPeople.zip;
 
     });
-    console.log(address + " " + city + " " + state + " " + zipcode);
+
     res.render('pages/fuelQuoteForm',
     {
         address: address,
@@ -219,25 +296,78 @@ app.post('/clientProfileName', function(req,res)
     });
  });
 
+//** MENU GET FUNCTION **//////////////////////////////////////////////////
 app.get('/menu', function(req, res){
     res.render('pages/menu');
 });
 
+//** LOGIN GET FUNCTION **//////////////////////////////////////////////////
 app.get('/login', function(req, res){
     res.render('pages/login');
 });
 
+//** CLIENT PROFILE NAME GET FUNCTION **//////////////////////////////////////////////////
 app.get('/clientProfileName', function(req, res){
     res.render('pages/clientProfileName');
 });
 
+//** FINAL SUBMISSION FORM GET FUNCTION **//////////////////////////////////////////////////
 app.get('/finalSubmissionForm', function(req, res){
-    res.render('pages/finalSubmissionForm');
+    
+    
+    var gallons = "";
+    var address  = "";
+    var suggPrice = "";
+    var total = "";
+
+    const copyOfCurrentUser = require('./currentUser.json');
+
+    copyOfCurrentUser.forEach(function(objPeople)
+    {
+        //username = objPeople.username;
+        gallons = objPeople.mostRecentGal;
+        address = objPeople.addrLine1;
+        suggPrice = objPeople.mostRecentSugPrice;
+        total = objPeople.mostRecentTotal;
+
+    });
+
+    
+    res.render('pages/finalSubmissionForm',
+    {
+        gallons: gallons,
+        address: address,
+        suggPrice: suggPrice,
+        total: total
+    });
 });
 
-app.get('/fuelQuoteHistory', function(req, res){
+//** FUEL QUOTE HISTORY GET FUNCTION **//////////////////////////////////////////////////
+app.get('/fuelQuoteHistory', function(req, res)
+{
+    // assigning the username that's found in currentUser
+    const copyOfCurrentUser = require('./currentUser.json');
+    var username = "";
+    copyOfCurrentUser.forEach(function(objPeople)
+    {
+        username = objPeople.username;
+    });
+
+    var query = { username: username };
+    db.collection("quotes").find(query).toArray(function(err, result) 
+    {
+        if (err) throw err;
+        console.log(result);
+        
+    });
     res.render('pages/fuelQuoteHistory');
 });
+
+//** CLIENT REGISTRATION GET FUNCTION **//////////////////////////////////////////////////
+app.get ('/clientRegistration', function(req, res){
+    res.render ('pages/clientRegistration');
+});
+
 
 app.get('/',function(req,res){
 res.set({
